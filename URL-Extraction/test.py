1fb10507d0423e9fb2d590de3faf9d86318b2ef0
HALF-BLOOD <khanalaayush1@gmail.com>
2023-03-11 14:02:40 +0545

    V3

--- URL-Extraction/test.py
+++ URL-Extraction/test.py
@@ -1,16 +1,38 @@
+from flask import Flask, request
 import pickle
 import pandas as pd
 import webUrl
 
 
-data=[]
-data.append(webUrl.featureExtraction("https://www.mail.beloinox.pl"))
+app = Flask(__name__)
 
-feature_names = ['Have_IP', 'Have_At', 'URL_Length', 'URL_Depth','Redirection', 
-                      'https_Domain', 'TinyURL', 'Prefix/suffix', 'DNS_Record', 'Web_Traffic', 
-                      'Domain_Age', 'Domain_End']
-data2 = pd.DataFrame(data, columns= feature_names)
+@app.route('/check-phishing', methods=['POST', 'OPTIONS'])
+def check_phishing():
+    if request.method == 'OPTIONS':
+        # Set CORS headers
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST',
+            'Access-Control-Allow-Headers': 'Content-Type'
+        }
+        return ('', 204, headers)
 
-model = pickle.load(open("XGBoostClassifier.pickle.dat", "rb"))
-p = model.predict(data2)
-print (p)
+    url = request.json['url']
+    print(f'Received URL: {url}')
+    
+    data=[]
+    data.append(webUrl.featureExtraction(url))
+
+    feature_names = ['Have_IP', 'Have_At', 'URL_Length', 'URL_Depth','Redirection', 
+                          'https_Domain', 'TinyURL', 'Prefix/suffix', 'DNS_Record', 'Web_Traffic', 
+                          'Domain_Age', 'Domain_End']
+    data2 = pd.DataFrame(data, columns= feature_names)
+
+    model = pickle.load(open("XGBoostClassifier.pickle.dat", "rb"))
+    p = model.predict(data2)
+    print (p)
+    
+    return ''
+
+if __name__ == '__main__':
+  app.run(debug=True)
