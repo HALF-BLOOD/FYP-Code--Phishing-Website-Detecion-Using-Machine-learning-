93f9ce58573faae10b1329c287ddc1b02b72e8ee
HALF-BLOOD <khanalaayush1@gmail.com>
2023-03-18 17:19:20 +0545

    URL-BLOCKLIST-V1

--- Google Extension/background.js
+++ Google Extension/background.js
@@ -1,16 +1,50 @@
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  if (message.action === "updatePhishingStatus") {
-    var isPhishing = message.isPhishing;
-    var message = message.message;
-    var result = document.getElementById('result');
-    var phishingStatus = document.getElementById('phishingStatus');
-    if (isPhishing) {
-      phishingStatus.style.color = "red";
-    } else {
-      phishingStatus.style.color = "green";
+// Set up a listener for web requests
+chrome.webRequest.onBeforeRequest.addListener(
+  function(details) {
+    // Check if the request matches a blocked URL
+    if (blockedUrls.includes(details.url)) {
+      console.log('Blocking URL:', details.url);
+      return {cancel: true};
     }
-    phishingStatus.textContent = message;
-    result.innerText = isPhishing ? 'This website is a phishing website.' : 'This website is not a phishing website.';
-    result.className = isPhishing ? 'result error' : 'result success';
+  },
+  {urls: ['<all_urls>']},
+  ['blocking']
+);
+
+// Make a request to the Flask server to check if the website is phishing
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  if (changeInfo.status === 'loading' && tab.url) {
+    fetch('http://localhost:5000/check-phishing', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({url: tab.url})
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.is_phishing) {
+        console.log('Phishing site detected:', tab.url);
+        blockedUrls.push(tab.url);
+        // Save the blocked URL to local storage
+        chrome.storage.local.set({blockedUrls: blockedUrls});
+        // Show a warning to the user
+        chrome.notifications.create({
+          type: 'basic',
+          iconUrl: 'Images/power.png',
+          title: 'Phishing Site Detected',
+          message: 'The website you are trying to access is a known phishing site and has been blocked.'
+        });
+      }
+    });
+  }
+});
+
+// Load the blocked URLs from local storage
+let blockedUrls = [];
+
+chrome.storage.local.get(['blockedUrls'], function(result) {
+  if (result.blockedUrls) {
+    blockedUrls = result.blockedUrls;
   }
 });
--- Google Extension/popup.js
+++ Google Extension/popup.js
@@ -1,59 +1,74 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var checkButton = document.getElementById('checkPhishing');
-  var result = document.getElementById('result');
-
-  checkButton.addEventListener('click', function() {
-    chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
-      var url = tabs[0].url;
-      var xhr = new XMLHttpRequest();
-      xhr.open("POST", "http://127.0.0.1:5000/check-phishing");
-      xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-      xhr.onload = function() {
-        if (xhr.status === 200) {
-          var response = JSON.parse(xhr.responseText);
-          var isPhishing = response.is_phishing;
-          result.innerText = isPhishing ? 'This website is a phishing website.' : 'This website is not a phishing website.';
-          result.className = isPhishing ? 'result error' : 'result success';
-        } else {
-          result.innerText = 'There was an error checking for phishing.';
-          result.className = 'result error';
-        }
-      };
-      xhr.send(JSON.stringify({url: url}));
+    var checkButton = document.getElementById('checkPhishing');
+    var result = document.getElementById('result');
+  
+    checkButton.addEventListener('click', function() {
+      chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
+        var url = tabs[0].url;
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "http://127.0.0.1:5000/check-phishing");
+        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhr.onload = function() {
+          if (xhr.status === 200) {
+            var response = JSON.parse(xhr.responseText);
+            var isPhishing = response.is_phishing;
+            result.innerText = isPhishing ? 'This website is a phishing website.' : 'This website is not a phishing website.';
+            result.className = isPhishing ? 'result error' : 'result success';
+  
+            // Block URL if it is a phishing website
+            if (isPhishing) {
+              chrome.tabs.update({url: 'chrome://blocked/'});
+            }
+          } else {
+            result.innerText = 'There was an error checking for phishing.';
+            result.className = 'result error';
+          }
+        };
+        xhr.send(JSON.stringify({url: url}));
+      });
     });
   });
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    var checkPhishingButton = document.getElementById('checkPhishingButton');
-    var phishingStatus = document.getElementById('phishingStatus');
   
-    checkPhishingButton.addEventListener('click', function() {
-      chrome.runtime.sendMessage({action: "getPhishingStatus"}, function(response) {
-        var isPhishing = response.isPhishing;
-        var message = response.message;
+  document.addEventListener('DOMContentLoaded', function() {
+      var checkPhishingButton = document.getElementById('checkPhishingButton');
+      var phishingStatus = document.getElementById('phishingStatus');
+    
+      checkPhishingButton.addEventListener('click', function() {
+        chrome.runtime.sendMessage({action: "getPhishingStatus"}, function(response) {
+          var isPhishing = response.isPhishing;
+          var message = response.message;
+          if (isPhishing) {
+            phishingStatus.style.color = "red";
+          } else {
+            phishingStatus.style.color = "green";
+          }
+          phishingStatus.textContent = message;
+  
+          // Block URL if it is a phishing website
+          if (isPhishing) {
+            chrome.tabs.update({url: 'chrome://blocked/'});
+          }
+        });
+      });
+    });
+    
+  
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+      if (request.action == "sendPhishingStatus") {
+        var isPhishing = request.isPhishing;
+        var message = request.message;
+        var phishingStatus = document.getElementById('phishingStatus');
         if (isPhishing) {
           phishingStatus.style.color = "red";
         } else {
           phishingStatus.style.color = "green";
         }
         phishingStatus.textContent = message;
-      });
-    });
-  });
   
-
-  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.action == "sendPhishingStatus") {
-      var isPhishing = request.isPhishing;
-      var message = request.message;
-      var phishingStatus = document.getElementById('phishingStatus');
-      if (isPhishing) {
-        phishingStatus.style.color = "red";
-      } else {
-        phishingStatus.style.color = "green";
+        // Block URL if it is a phishing website
+        if (isPhishing) {
+          chrome.tabs.update({url: 'chrome://blocked/'});
+        }
       }
-      phishingStatus.textContent = message;
-    }
-  });
+    });
   
