9d21696f3a990b173d85fa58e49e9f455be9467c
HALF-BLOOD <khanalaayush1@gmail.com>
2023-03-19 23:14:43 +0545

    CSS-JS V1

--- Google Extension/popup.js
+++ Google Extension/popup.js
@@ -1,63 +1,56 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var checkButton = document.getElementById('checkPhishing');
-    var result = document.getElementById('result');
-  
-    checkButton.addEventListener('click', function() {
-      chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
-        var url = tabs[0].url;
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", "http://127.0.0.1:5000/check-phishing");
-        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-        xhr.onload = function() {
-          if (xhr.status === 200) {
-            var response = JSON.parse(xhr.responseText);
-            var isPhishing = response.is_phishing;
-            result.innerText = isPhishing ? 'This website is a phishing website.' : 'This website is not a phishing website.';
-            result.className = isPhishing ? 'result error' : 'result success';
-  
-            // Block URL if it is a phishing website
-            if (isPhishing) {
-              chrome.tabs.update({url: 'chrome://blocked/'});
-            }
-          } else {
-            result.innerText = 'There was an error checking for phishing.';
-            result.className = 'result error';
-          }
-        };
-        xhr.send(JSON.stringify({url: url}));
-      });
-    });
-  });
-  
-  document.addEventListener('DOMContentLoaded', function() {
-      var checkPhishingButton = document.getElementById('checkPhishingButton');
-      var phishingStatus = document.getElementById('phishingStatus');
-    
-      checkPhishingButton.addEventListener('click', function() {
-        chrome.runtime.sendMessage({action: "getPhishingStatus"}, function(response) {
-          var isPhishing = response.isPhishing;
-          var message = response.message;
-          if (isPhishing) {
-            phishingStatus.style.color = "red";
-          } else {
-            phishingStatus.style.color = "green";
-          }
-          phishingStatus.textContent = message;
-  
+  var checkButton = document.getElementById('checkPhishing');
+  var result = document.getElementById('result');
+  var urlDisplay = document.getElementById('url-display');
+  var phishingButtons = document.getElementById('phishing-buttons'); // new element for phishing buttons
+  var blocklist = document.getElementById('blocklist-remove'); // new element for report phishing button
+
+  checkButton.addEventListener('click', function() {
+    chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
+      var url = tabs[0].url;
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", "http://127.0.0.1:5000/check-phishing");
+      xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+      xhr.onload = function() {
+        if (xhr.status === 200) {
+          var response = JSON.parse(xhr.responseText);
+          var isPhishing = response.is_phishing;
+          result.innerText = isPhishing ? 'This website is a phishing website.' : 'This website is not a phishing website.';
+          result.className = isPhishing ? 'result error' : 'result success';
+
           // Block URL if it is a phishing website
           if (isPhishing) {
             chrome.tabs.update({url: 'chrome://blocked/'});
+
+            // Show phishing buttons if the result is phishing
+            phishingButtons.style.display = "block";
           }
-        });
-      });
+        } else {
+          result.innerText = 'There was an error checking for phishing.';
+          result.className = 'result error';
+        }
+      };
+      xhr.send(JSON.stringify({url: url}));
+
+      // Display the URL in the popup
+      urlDisplay.textContent = 'Checking URL: ' + url;
     });
-    
+  });
+
+  blocklist.addEventListener('click', function() {
+    // Do something when "Report Phishing" button is clicked
+  });
+});
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var checkPhishingButton = document.getElementById('checkPhishingButton');
+    var phishingStatus = document.getElementById('phishingStatus');
   
-    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-      if (request.action == "sendPhishingStatus") {
-        var isPhishing = request.isPhishing;
-        var message = request.message;
-        var phishingStatus = document.getElementById('phishingStatus');
+    checkPhishingButton.addEventListener('click', function() {
+      chrome.runtime.sendMessage({action: "getPhishingStatus"}, function(response) {
+        var isPhishing = response.isPhishing;
+        var message = response.message;
         if (isPhishing) {
           phishingStatus.style.color = "red";
         } else {
@@ -69,6 +62,37 @@ document.addEventListener('DOMContentLoaded', function() {
         if (isPhishing) {
           chrome.tabs.update({url: 'chrome://blocked/'});
         }
-      }
+      });
     });
+  });
   
+  
+  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action == "sendPhishingStatus") {
+      var isPhishing = request.isPhishing;
+      var message = request.message;
+      var phishingStatus = document.getElementById('phishingStatus');
+      if (isPhishing) {
+        phishingStatus.style.color = "red";
+      } else {
+        phishingStatus.style.color = "green";
+      }
+      phishingStatus.textContent = message;
+  
+      // Block URL if it is a phishing website
+      if (isPhishing) {
+        chrome.tabs.update({url: 'chrome://blocked/'});
+      }
+    }
+  });
+  
+
+  // ---------------visual js----------------
+  const toggleButton = document.getElementById("toggleExtension");
+      toggleButton.addEventListener("click", function() {
+        toggleButton.classList.toggle("on");
+        toggleButton.classList.toggle("off");
+        toggleButton.textContent = toggleButton.classList.contains("on") ? "ON" : "OFF";
+      });
+
+      
