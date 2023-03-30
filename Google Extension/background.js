f7b6422456592f9b6dafb5ea7c6e3e962f134bbd
HALF-BLOOD <khanalaayush1@gmail.com>
2023-03-30 13:19:44 +0545

    Project complete set 1

--- Google Extension/background.js
+++ Google Extension/background.js
@@ -1,47 +1,28 @@
+// background.js
 
-// Store the blocked URLs in an array
-var blockedUrls = [];
-
-// Add a listener for webRequest events
-chrome.webRequest.onBeforeRequest.addListener(
-  function(details) {
-    // Check if the URL is in the blockedUrls array
-    if (blockedUrls.indexOf(details.url) !== -1) {
-      // Cancel the request and redirect to the blocked page
-      return {cancel: true, redirectUrl: chrome.extension.getURL("blocked.html")};
-    }
-  },
-  {urls: ["<all_urls>"]},
-  ["blocking"]
-);
-
-// Add a listener for messages from the popup
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.action === "addBlockedUrl") {
-    // Add the URL to the blockedUrls array
-    blockedUrls.push(request.url);
-    // Save the blockedUrls array to local storage
-    chrome.storage.local.set({blockedUrls: blockedUrls}, function() {
-      console.log("Blocked URLs saved.");
-    });
-  } else if (request.action === "getBlockedUrls") {
-    // Get the blockedUrls array from local storage
-    chrome.storage.local.get("blockedUrls", function(result) {
-      if (result.blockedUrls) {
-        blockedUrls = result.blockedUrls;
-      }
-      // Send the blockedUrls array back to the popup
-      sendResponse({blockedUrls: blockedUrls});
-    });
+chrome.webNavigation.onCommitted.addListener((details) => {
+  if (details.frameId === 0) {
+    const url = details.url;
+    checkForPhishing(url);
   }
 });
 
+function checkForPhishing(url) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "http://127.0.0.1:5000/check-phishing");
+  xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      var response = JSON.parse(xhr.responseText);
+      var isPhishing = response.is_phishing;
 
-blocklist.addEventListener('click', function() {
-  chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
-    var url = tabs[0].url;
-    chrome.runtime.sendMessage({action: "addBlockedUrl", url: url}, function(response) {
-      console.log(response.message);
-    });
-  });
-});
+      if (isPhishing) {
+        // Block the phishing website
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+          chrome.tabs.update(tabs[0].id, { url: "chrome://blocked/" });
+        });
+      }
+    }
+  };
+  xhr.send(JSON.stringify({ url: url }));
+}
